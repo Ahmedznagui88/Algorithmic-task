a431375b6bcea2c4b6772b1641a419225b2ca05b
Ahmed Znagui <amed.bob88@gmail.com>
2026-07-19 21:19:15 +0200

    Calculate the depth and height of each node in the tree

--- task2/btree.cpp
+++ task2/btree.cpp
@@ -21,6 +21,18 @@ void flip(node* &tree) {
     }
 }
 
+int max(int leftHeight, int rightHeight){
+    return (leftHeight > rightHeight) ? leftHeight : rightHeight;
+}
+
+int treeHeight(node* tree){
+    if(tree == nullptr) return 0;
+    return 1 + max(treeHeight(tree->left), treeHeight(tree->right));
+}
+
+void treeDepth(node* tree, int currentDepth){
+    
+}
 
 int main(){
     return 0;

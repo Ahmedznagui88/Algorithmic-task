392a1653a4e51aaa7698f6219c4a241bb889a8e3
Ahmed Znagui <amed.bob88@gmail.com>
2026-07-19 19:21:03 +0200

    done with flip function

--- task2/btree.cpp
+++ task2/btree.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+
 struct node{
     int data;
     node* right;
     node* left;
 };
 
-int flip(){
+void flip(node* &tree) {
+    node* tem;
 
+    if(tree == nullptr) {
+        return;
+    }else {
+        tem = tree->right;
+        tree->right = tree->left;
+        tree->left = tem;
+
+        flip(tree->left);
+        flip(tree->right);
+    }
 }
 
 
